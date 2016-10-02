6464b306a84beb0cdc060fa4de457b8e7de6f1e0
Travis Cunningham <tcunningham@smartfile.com>
2016-10-02 16:35:48 -0400

    Remove init

--- django_zip_stream/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Assemble ZIP archives dynamically using Nginx."""
-import pkg_resources
-
-
-#: Module version, as defined in PEP-0396.
-__version__ = pkg_resources.get_distribution(__package__.replace('-', '_')) \
-                           .version
