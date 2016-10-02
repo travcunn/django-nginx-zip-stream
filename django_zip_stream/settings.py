7711ab6470c0ba3073c9391bcea0e13eb2d30c5e
Travis Cunningham <tcunningham@smartfile.com>
2016-10-02 11:18:17 -0400

    Add test coverage. Closes #6

--- django_zip_stream/settings.py
+++ django_zip_stream/settings.py
@@ -7,4 +7,12 @@ DATABASES = {
 
 SECRET_KEY = '8ij$(7l2!w0f8kggntbv=+$bd=^2xs$cl+3v^_##qjw@2py9_3'
 
-INSTALLED_APPS = ('django_zip_stream', )
+INSTALLED_APPS = ('django_zip_stream', 'django_nose')
+
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=django_zip_stream',
+]
